8f2e4992ae643df38f56c79b4d44a4ad019f80a8
sichengwen <sichengwen@kuaishou.com>
2023-08-08 14:12:41 +0800

    add log

--- contracts/UsdcToken.sol
+++ contracts/UsdcToken.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract UsdcToken is ERC20 {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        string msg = "mint to" + msg.sender + " 20000";
+        console.logString(msg);
         _mint(msg.sender, 20000);
     }
 }
